5ad718c1e1d8d23e4d00008543150a37f6c0abb0
James.Sampedro <72242960+Votum38git@users.noreply.github.com>
2020-10-16 19:55:57 +0800

    Initial change

---
+++ ADETQ3_James_Sampedro/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ADETQ3_James_Sampedro.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
